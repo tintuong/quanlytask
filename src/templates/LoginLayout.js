701728f59292a4818e325b939ca2e76a84df2ce5
Tin Tuong <tintuong1009pt@gmail.com>
2022-03-24 07:47:27 +0700

    first commit

---
+++ src/templates/LoginLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+export default function LoginLayout(props) {
+    let {Component,...rest}=props
+  return (
+    <Route {...rest} render={(propsRoute)=>{
+        return <Component {...propsRoute} />
+    }} />
+  )
+}
